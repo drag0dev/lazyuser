b43419fc012b3e0ec82c286ed9f2969ec7cf0307
Dragoslav Tamindzija <dragodev@protonmail.com>
2022-01-24 21:48:06 +0100

    header component

--- front_end/src/components/Header.tsx
+++ front_end/src/components/Header.tsx
@@ -1,9 +1,40 @@
-import React  from 'react';
+import React from 'react';
+import {useState, useEffect} from 'react';
+import URIs from '../ApiURIs';
+interface userInfoInterface{
+    username: string
+    logged: boolean
+}
 
 const Header = () => {
+    const [userInfo, setUserInfo] = useState<userInfoInterface>({username: '', logged: false});
+
+    const checkLogged = async() => {
+        let res = await fetch(URIs.urlCheckLogin, {
+            method: 'POST',
+            credentials: 'include'
+        });
+        
+        if(res.status==200){
+            let data;
+            data = await res.json();
+            setUserInfo({username: data.username, logged: true});
+        }
+    }  
+
+    useEffect(()=>{
+        checkLogged();
+    }, []);
+    
+
     return (
-        <div>
-            <h1>Lazy User</h1>
+        <div className='navbar'>
+            <h1 className='appTitle'>Lazy User</h1>
+            <div className='userInteractionNavBar'>
+                {userInfo.logged && <div><p>{`Welcome, ${userInfo.username}`}</p></div>}
+                {userInfo.logged && <div><a>Settings</a></div>}
+                {userInfo.logged ? <div><a>Log out</a></div>: <div><a>Log in</a></div>}
+            </div>
         </div>
     )
 }
